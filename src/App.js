a9911ec0cc85c5c287fc0f9662172bce44c18659
ilyaszm <zmr.ilyas@gmail.com>
2019-07-17 23:25:43 +0100

    Adding the loadMoreRepos Method & Infinite Pagination when Scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import InfiniteScroll from "react-infinite-scroll-component"
 
 import Repo from './components/Repo'
 import './App.css'
@@ -8,13 +9,15 @@ export default function App() {
 
     const [repos, setRepos] = useState([])
     const [error, setError] = useState(false)
+    // set page to number "2" to use it in the next fetch
+    const [page, setPage] = useState(2)
 
     /*  Fetch repos from Github API  */
     useEffect(() => {
         setError(false)
         try {
             axios
-                // by default the page value here is set to "1", so we don't need to add page parameter
+                // by default the page value is "1"
                 .get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
                 // store the fetched data into repos []
                 .then(result => { setRepos(result.data.items) })
@@ -23,25 +26,50 @@ export default function App() {
         // passing an empty [] to let repos state inside the hook always have the initial value
     }, [])
 
+    /*  Fetching repos from the next page  */
+    const loadMoreRepos = () => {
+        setError(false)
+        try {
+            axios
+                .get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`)
+                .then( result => {
+                    // Adding the new fetched repos to the first ones
+                    setRepos(repos.concat(result.data.items))
+                    setPage(page + 1)
+                })
+        }
+        catch (error) { setError(true) }
+    }
+
     return (
         <div>
-            <h1 className="text-center display-4">Github Repos</h1>
+            <h1 className="text-center display-4 pt-5">Github Repos</h1>
             <hr style={hr_style} />
 
             {/*  check if any errors to handel  */}
-            { error && <h5 className="text-center">Something went wrong...!!</h5> }
+            { error && <h5 className="text- mt-5">Something went wrong...!!</h5> }
 
-            {
-                // mapping through the repos array to display the fetched data
-                repos.map(repo => (
-                    <Repo
-                        // key makes every repo unique and doesn't affect to others
-                        key = { repo.id }
-                        // passing repo as a prop to use data from repos[] in the Repo component
-                        repo = { repo }
-                    />
-                ))
-            }
+            {/*  Auto Pagination when scrolling by using InfiniteScroll Component  */}
+            <InfiniteScroll
+                // define "dataLength" to make the upcoming calls to "next" goes successfully
+                dataLength  = { repos.length }
+                // the loadMoreRepos() will be called after we reach the bottom
+                next        = { loadMoreRepos }
+                // "hasMore" tells the InfiniteScroll to call "next" after reaching the bottom
+                hasMore     = { true }
+                loader      = { <h3 className="text-center mb-5">Loading...</h3> }
+            >
+                {
+                    // mapping through the repos array to display the fetched data
+                    repos.map(repo => (
+                        <Repo
+                            key = { repo.id }
+                            // passing repo as a prop to use data from repos[] in the Repo component
+                            repo = { repo }
+                        />
+                    ))
+                }
+            </InfiniteScroll>
 
         </div>
     )
